30d1f9e05acfc410db435c93be000d1bdfd1ac09
Leandro Breda <leandrobreda1@gmail.com>
2024-05-14 15:21:51 -0300

    Ajustes projeto Criando um sistema abstraindo um celular POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("9999999", "Nokia teste", "1111111", 1024);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone("8888888", "Iphone 15", "23232323", 2048);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegran");
